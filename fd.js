5eff629cecca9c89902cfa147d55469bf37c44f1
Pittawatt <124548012+Pittawatt@users.noreply.github.com>
2023-05-29 11:08:19 +0700

    Add files via upload

---
+++ fd.js
@@ -0,0 +1,145 @@
+var fd = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ซ้งเป็ดพะโล้ วังหิน",
+        "type": 4.3
+      },
+      "geometry": {
+        "coordinates": [
+          100.59245467398483,
+          13.830591201269158
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เฝอ 54",
+        "type": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.59170759504889,
+          13.822018752156879
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ขาหมูคุโรบุตะ 5รางวัลอร่อย",
+        "type": "3.0"
+      },
+      "geometry": {
+        "coordinates": [
+          100.59153325392583,
+          13.820358605727094
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "โกวนัน แซ่บถึงใจ",
+        "type": "4.0"
+      },
+      "geometry": {
+        "coordinates": [
+          100.60089400708108,
+          13.831689938063008
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wood Cafe",
+        "type": 4.3
+      },
+      "geometry": {
+        "coordinates": [
+          100.59393676925498,
+          13.818734717390512
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เลิศทิพย์ วังหิน ปูทะเลไข่ดอง",
+        "type": 3.7
+      },
+      "geometry": {
+        "coordinates": [
+          100.59180658693361,
+          13.82759647547816
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ราชาบะหมี่เกี๊ยว วังไหิน",
+        "type": 4.2
+      },
+      "geometry": {
+        "coordinates": [
+          100.5907694661567,
+          13.807259074779395
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "โรตีลาดพร้าววังหิน 31",
+        "type": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          100.59032399369335,
+          13.806810602842063
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ก๋วยเตี๋ยวต้มยำไข่ลาวา",
+        "type": "4.0"
+      },
+      "geometry": {
+        "coordinates": [
+          100.59163588565008,
+          13.827020229066974
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nanalee korean BBQ",
+        "type": 4.2
+      },
+      "geometry": {
+        "coordinates": [
+          100.59185745658044,
+          13.83180864870775
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
